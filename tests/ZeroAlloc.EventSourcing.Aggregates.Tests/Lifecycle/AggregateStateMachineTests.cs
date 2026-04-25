d438924877b76abb82e04a6cca2fc5cc02c1cd95
Marcel Roozekrans <marcel@roozekrans.nl>
2026-04-25 18:18:07 +0200

    test: add empty AggregateStateMachineTests scaffold (#67)

---
+++ tests/ZeroAlloc.EventSourcing.Aggregates.Tests/Lifecycle/AggregateStateMachineTests.cs
@@ -0,0 +1,9 @@
+using FluentAssertions;
+using ZeroAlloc.EventSourcing;
+
+namespace ZeroAlloc.EventSourcing.Aggregates.Tests.Lifecycle;
+
+public class AggregateStateMachineTests
+{
+    // Test cases added in Tasks 7-12 below.
+}
